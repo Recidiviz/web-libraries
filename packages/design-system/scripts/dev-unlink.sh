3ba2a77a7be821f2fef9b775afa8cfc4a89e82a3
Dan Hansen <dan@recidiviz.org>
2021-06-10 11:02:31 -0700

    Add convenience scripts for link/unlink

---
+++ packages/design-system/scripts/dev-unlink.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+DEPENDENT_PROJECT=$1
+if [ -d $DEPENDENT_PROJECT ]; then
+  echo "Unlinking design-system from ${DEPENDENT_PROJECT}"
+else
+  echo "Error: Directory ${DEPENDENT_PROJECT} does not exist"
+  exit 1
+fi
+pushd $1
+yarn unlink @recidiviz/design-system react react-modal
+popd
+yarn unlink
+pushd node_modules
+pushd react && yarn unlink && popd
+pushd react-modal && yarn unlink && popd
+popd
+
