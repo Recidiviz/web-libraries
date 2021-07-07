dbca3dd33c8187e46817d6ce81918cd87876eaa9
Ian MacFarland <macfarlandian@users.noreply.github.com>
2021-07-07 16:15:37 -0700

    [ESLint] Enforce import sort order (#41)

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -90,6 +90,8 @@ module.exports = {
       "error",
       { declaration: false, assignment: false },
     ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
   overrides: [
     {
