e416816d8dfcb1e657c25363bf117c522adb02d7
Ian MacFarland <ian@recidiviz.org>
2022-05-25 15:21:09 -0700

    add scss mixins

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,12 +1,21 @@
-const path = require("path");
-
 module.exports = {
   // The stories array sorts the story list
   stories: [
     "../src/stories/Pages/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+        },
+      },
+    },
+  ],
   typescript: {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
@@ -15,14 +24,5 @@ module.exports = {
         esModuleInterop: false,
       },
     },
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../"),
-    });
-
-    return config;
   },
 };
