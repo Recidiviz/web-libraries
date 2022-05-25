ba246d3c6d2f1037d7c458dab0e6b69746bd3e69
Ian MacFarland <ian@recidiviz.org>
2022-05-25 10:28:29 -0700

    add components derived from SCSS

--- packages/design-system/src/components/Typography/UI.ts
+++ packages/design-system/src/components/Typography/UI.ts
@@ -14,33 +14,36 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
-import { Meta, Story } from "@storybook/react";
-import * as React from "react";
-import {
-  TitleXXL,
-  TitleXL,
-  Subtitle,
-  H1,
-  H2,
-  H3,
-  H4,
-} from "../../components/Typography";
 
-export default {
-  title: "Design System/Pages/Typography",
-} as Meta;
+import styled from "styled-components";
+import { styles } from "./styles";
 
-const Template: Story = () => (
-  <div>
-    <TitleXXL>Title/Heading XXL</TitleXXL>
-    <TitleXL>Title/Heading XL</TitleXL>
-    <Subtitle>Subtitle</Subtitle>
-    <H1>Heading 1</H1>
-    <H2>Heading 2</H2>
-    <H3>Heading 3</H3>
-    <H4>Heading 4</H4>
-    <TitleXL as="h6">`as` polymorphic h6 tag</TitleXL>
-  </div>
-);
+export const Sans12 = styled.div`
+  ${styles.Sans12}
+`;
+Sans12.displayName = "Sans12";
 
-export const Typography = Template.bind({});
+export const Sans14 = styled.div`
+  ${styles.Sans14}
+`;
+Sans14.displayName = "Sans14";
+
+export const Sans16 = styled.div`
+  ${styles.Sans16}
+`;
+Sans16.displayName = "Sans16";
+
+export const Sans24 = styled.div`
+  ${styles.Sans24}
+`;
+Sans24.displayName = "Sans24";
+
+export const Serif24 = styled.div`
+  ${styles.Serif24}
+`;
+Serif24.displayName = "Serif24";
+
+export const Serif34 = styled.div`
+  ${styles.Serif34}
+`;
+Serif34.displayName = "Serif34";
