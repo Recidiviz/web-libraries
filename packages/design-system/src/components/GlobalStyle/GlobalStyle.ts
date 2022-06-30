be56389af2038f2d30f1952c5d70c10a2172e314
Ian MacFarland <ian@recidiviz.org>
2022-06-29 17:21:05 -0700

    remove fontsource Libre Franklin dependency

--- packages/design-system/src/components/GlobalStyle/GlobalStyle.ts
+++ packages/design-system/src/components/GlobalStyle/GlobalStyle.ts
@@ -21,8 +21,6 @@ import "@fontsource/public-sans/400.css";
 import "@fontsource/public-sans/500.css";
 import "@fontsource/public-sans/600.css";
 import "@fontsource/public-sans/700.css";
-import "@fontsource/libre-franklin";
-import "@fontsource/libre-franklin/500.css";
 import "@fontsource/libre-baskerville";
 
 export const GlobalStyle = createGlobalStyle`
